ee95aaea0e29efba0fae43830e6c025a7dc0dd92
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2023-11-04 18:41:58 -0700

    Added date tracking to server

--- server.py
+++ server.py
@@ -1,12 +1,29 @@
 from flask import Flask
+import re
+import datetime
+
+x = datetime.datetime.now()
 
 app = Flask(__name__)
 
+# python -m server run
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+# add this keyword to url outputted in terminal to trigger this function to output
+@app.route('/data')
+def get_time():
+    return {
+        'Name': "geek",
+        "Age": "22",
+        "Date": x,
+        "programming": "python"
+    }
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
