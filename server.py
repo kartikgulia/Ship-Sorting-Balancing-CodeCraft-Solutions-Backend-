1480675f8752272d9de033c170433749b3208aab
RayyanZaid <rayyanzaid0401@gmail.com>
2023-11-07 12:29:57 -0800

    can recieve data from frontend

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import re
 import datetime
 
 from flask_cors import CORS
 
-x = datetime.datetime.now()
 
 app = Flask(__name__)
 
 # Configure CORS to allow requests from http://localhost:3000
-CORS(app, resources={r"/data": {"origins": "http://localhost:3000"}})
+CORS(app, resources={
+
+    r"/data": {"origins": "http://localhost:3000"},
+    r"/sendName": {"origins": "http://localhost:3000"}
+
+})
 
 # python -m server run
 
@@ -22,12 +26,22 @@ def home():
 # add this keyword to url outputted in terminal to trigger this function to output
 @app.route('/data')
 def get_time():
+    x = datetime.datetime.now()
     return {
         'Name': "geek",
         "Age": "22",
         "Date": x,
         "programming": "python"
     }
+
+
+@app.route('/sendName', methods=['POST'])
+def send_name():
+    if request.method == 'POST':
+        data = request.json  # Assuming data is sent as JSON
+        name = data.get('name')
+        print("Received name:", name)
+        return jsonify({'message': 'Name received successfully'})
 
 
 if __name__ == '__main__':
