92424d65bce5b19f6522ecb9a3f7399458508ac3
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2023-11-14 11:03:42 -0800

    Added a delimiter to read_csv to fix white space issues in dataframe

--- data.py
+++ data.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plt
+
 
 # set this equal to name of txt file. Might need to change this depending on how the frontend will send the text file to the backend
 manifest = "manifest.txt"
 headers = ['Position', 'Weight', 'Cargo']
-data = pd.read_csv(manifest, names=headers)
+data = pd.read_csv(manifest, sep=', ', names=headers)
 print(data)
+
+# might need to iterate through database and convert position and weight into numerical values
+# print(data.iloc[1])
