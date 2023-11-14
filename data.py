76ad811995ac70f2211968c57f94e0182192166c
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2023-11-14 10:24:48 -0800

    Added a comment

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-manifest = "manifest.txt"  # set this equal to name of txt file
+# set this equal to name of txt file. Might need to change this depending on how the frontend will send the text file to the backend
+manifest = "manifest.txt"
 headers = ['Position', 'Weight', 'Cargo']
 data = pd.read_csv(manifest, names=headers)
 print(data)
