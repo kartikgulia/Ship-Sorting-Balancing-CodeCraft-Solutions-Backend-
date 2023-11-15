d32afc5f5f860c804288935b8f285fa3f5b84157
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2023-11-14 19:37:03 -0800

    Code now creates an array

--- data.py
+++ data.py
@@ -6,12 +6,7 @@ import matplotlib as plt
 class Cargo:
     position = [0, 0]
     weight = 0
-    name = ""
-
-    def init(input, cargo_description):
-        position = input[0]
-        weight = input[1]
-        name = cargo_description
+    name = "NAN"
 
 
 def conversion(position, weight):  # converts string data to numbers
@@ -44,12 +39,28 @@ def conversion(position, weight):  # converts string data to numbers
 # set this equal to name of txt file. Might need to change this depending on how the frontend will send the text file to the backend
 manifest = "manifest.txt"
 headers = ['Position', 'Weight', 'Cargo']
-data = pd.read_csv(manifest, sep=', ', names=headers)
+data = pd.read_csv(manifest, sep=', ', names=headers, engine='python')
 print(data)
 
-# all positions and weights are strings. need to convert to numbers. planning to ignore the zero column and zero row
-cargo_grid = [[Cargo] * 12] * 8
+# cargo_grid = [[Cargo] * 13] * 9
+# made a smaller 2D array since manifest text file only has eight slots rght now. Takes in 9 slots to fill the 3x3 portion of the array while ignoring zero row and zero column
+cargo_grid = [[Cargo] * 4] * 4
 
-# print(conversion(data.at[7, 'Position'], data.at[7, 'Weight']))
-for x in data.index:
-    print(conversion(data.at[x, 'Position'], data.at[x, 'Weight']))
+
+# had some issues with filling the regular 2D array with just eight values. Need to make a new text file that would fit all 8x12 slots (Might be a way to get python to generate a random manifest for us).
+# this algorithm basically fills the entire array. just need to change size in line of code above to the appropriate size once we have a proper manifest file to use.
+i = 0
+for x in cargo_grid:
+    if (x == 0):
+        continue
+    for y in x:
+        if (y == 0):
+            continue
+        position_weight = conversion(
+            data.at[i, 'Position'],  data.at[i, 'Weight'])
+        y.name = data.at[i, 'Cargo']
+        y.position = position_weight[0]
+        y.weight = position_weight[1]
+        print(y.name, " ",
+              y.position, " ", y.weight)
+        i += 1
