c5de287ed2be5b17f220420fc294f1bc5452eae6
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2023-11-25 16:03:40 -0800

    Made a function to get to get lowest position we can place cargo in for a given column

--- Balance.py
+++ Balance.py
@@ -49,10 +49,25 @@ class Balance:
                         if (self.cargo_grid[x][y].name != "NAN" and self.cargo_grid[x][y].name != "UNUSED"):
                             self.cargoList.append(self.cargo_grid[x][y])
 
+    # want to get lowest position we can place cargo in for a given column
+    def lowestPosition(self, column):
+        if (self.cargo_grid[1][column].name == "UNUSED"):
+            return [1, column]
+        cargo_column = [row[column] for row in self.cargo_grid]
+        for x in cargo_column:
+            if (x.name == "UNUSED"):
+                return self.cargo_grid[x][column].position
+        """
     def Balance(self):
+        if(self.Balance_Check):
+            self.CargoList() # if ship container is unbalanced, make a list of all containers on the heavier side
+            for x in self.cargoList:                              
+        else:
+            return
         for x in range(len(self.cargo_grid)):
             if (x == 0):
                 continue
             for y in range(len(self.cargo_grid[x])):
                 if (y == 0):
                     continue
+        """
