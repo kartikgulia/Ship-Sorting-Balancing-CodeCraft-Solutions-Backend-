df6a1f63f6d921c9d2141bde079fcc322b37685e
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2023-11-28 12:15:04 -0800

    Modified output

--- Balance.py
+++ Balance.py
@@ -63,7 +63,7 @@ class Balance:
                 pass
                 output = ""
             self.ProgressionList.append(self.CargoGrid.cargo_grid)
-            # if ship container is unbalanced, make a list of all containers on the heavier side
+            # if ship container is unbalanced, make a list of all containers
             self.CargoList()
             while (balanced == False):
                 # cargo we put crane over. goes from top row to bottom row, right to left
@@ -79,17 +79,14 @@ class Balance:
                     self.nodeList, reverse=True, key=lambda x: x.Weight_Ratio)  # sort node list by how large weight ratio is
                 self.CargoGrid.Grid_Copy(  # set cargo grid to grid wth largest weight ratio
                     self.nodeList[0])
-                output += "Move cargo " + \
-                    "from ( " + str(self.CargoGrid.old_pos[0]) + "," + str(self.CargoGrid.old_pos[1]) + \
-                    ")  to (" + str(self.CargoGrid.new_pos[0]) + \
-                    "," + \
-                    str(self.CargoGrid.new_pos[1]) + ")" + "\n"
+
+                output += f"Move cargo from ({str(self.CargoGrid.old_pos[0])},{str(self.CargoGrid.old_pos[1])}) to ({str(self.CargoGrid.new_pos[0])},{str(self.CargoGrid.new_pos[1])})\n"
                 self.ProgressionList.append(
-                    copy.deepcopy(self.CargoGrid))
+                    self.nodeList[0])
                 # check if new cargo grid is balanced
                 if (self.CargoGrid.Balance_Check() == True):
-                    # with open(file, "w") as file:  # output operations list
-                    # file.write(output)
+                    with open(filename, "w") as file:  # output operations list
+                        file.write(output)
                     balanced = True
 
         else:  # already balanced
