6331e67b55140067955ac1410f1a86853547c588
RayyanZaid <rayyanzaid0401@gmail.com>
2023-11-29 19:41:51 -0800

    done

--- manifestAccess.py
+++ manifestAccess.py
@@ -15,25 +15,39 @@ def getManifestName():
     return old_manifest_name
 
 
-def getManifestGridHelper(manifest_name) -> List[Dict[str, Any]]:
+def getManifestGridHelper(manifest_path):
     headers = ['Position', 'Weight', 'Cargo']
     pandasDF_for_Manifest = pd.read_csv(
-        f'./ManifestInformation/{manifest_name}', sep=', ', names=headers, engine='python')
+        f'{manifest_path}', sep=', ', names=headers, engine='python')
 
     # Initialize and populate the Cargo_Grid
     cargo_grid = Cargo_Grid(pandasDF_for_Manifest)
     cargo_grid.array_builder()
 
     # Convert the Cargo_Grid to a JSON-serializable format
-    grid_data = []
-    for x in range(len(cargo_grid.cargo_grid)):
-        for y in range(len(cargo_grid.cargo_grid[x])):
-            cargo = cargo_grid.cargo_grid[x][y]
-            grid_data.append({
+    grid_data = [{}] * 8
+    for i in range(8):
+        grid_data[i] = [{}] * 12
+
+    for eachRow in range(len(cargo_grid.cargo_grid)):
+        for eachCol in range(len(cargo_grid.cargo_grid[eachRow])):
+
+            cargo = cargo_grid.cargo_grid[eachRow][eachCol]
+
+            position = cargo.position
+            # print(position)
+
+            if position == [0, 0]:
+                continue
+
+            rowNum = 8 - position[0]
+            colNum = position[1] - 1
+
+            grid_data[rowNum][colNum] = {
                 "position": cargo.position,
-                "weight": cargo.weight,
-                "name": cargo.name
-            })
+                "name": cargo.name,
+                "weight": cargo.weight
+            }
 
     return grid_data
 
